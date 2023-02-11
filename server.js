fa24baabcd3b5623edbd113f1a626199ccbe4790
Tyler Moore <tymo821@aol.com>
2023-02-11 17:12:08 -0500

    updated server.js logic and comments

--- server.js
+++ server.js
@@ -1,17 +1,22 @@
+// Import the express library
 const express = require('express');
+// Import the database connection
 const sequelize = require('./config/connection');
+// Import the routes
 const routes = require('./routes');
-// import sequelize connection
+
 
 const app = express();
+// Define the port the application will run on
 const PORT = process.env.PORT || 3001;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+// Use the imported routes in our application
 app.use(routes);
 
-// sync sequelize models to the database, then turn on the server
+// Sync the models with the database and start the express server
 sequelize.sync({ force: false }).then(() => {
   app.listen(PORT, () => console.log(`App listening on port ${PORT}!`));
 });
